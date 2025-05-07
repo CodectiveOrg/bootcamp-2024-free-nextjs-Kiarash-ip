a6b72c9527607ac0d939351e752ba4da3161bd27
Kiarash <kia.iranpour1380@gmail.com>
2025-05-07 18:25:26 +0330

    [FIX] remove typescript ts-ignores

--- src/dto/auth.dto.ts
+++ src/dto/auth.dto.ts
@@ -1,6 +1,4 @@
 import Prisma from "@prisma/client";
 
-// @ts-ignore
 export type SignUpDto = Omit<Prisma.User, "id">;
-// @ts-ignore
 export type SignInDto = Pick<Prisma.User, "username" | "password">;
